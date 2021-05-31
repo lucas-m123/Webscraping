3013a987232b442885394c81c6286dfbd87376a8
Lucas <lucas.marchetti.unlz@gmail.com>
2021-05-31 01:54:15 -0300

    Función para guardar todas las imágenes del Instagram. AÚN FALTA PULIR

--- Scrap Instagram.py
+++ Scrap Instagram.py
@@ -11,9 +11,10 @@ import wget
 
 # -----WEBDRIVER----- #
 ruta_driver = "C:/Users/John Salchichón/Desktop/Práctica/Python/chromedriver.exe"
-# Las lineas 15 y 16 son para usar el navegador en modo incógnito, no son obligatorias
+# Estas dos lineas son para usar el navegador en modo incógnito, no son obligatorias
 incognito = webdriver.ChromeOptions()
 incognito.add_argument("--incognito")
+
 nav = webdriver.Chrome(executable_path=ruta_driver, options=incognito)
 nav.get("https://instagram.com/")
 
@@ -25,12 +26,14 @@ contraseña.clear()
 usuario.send_keys("") # Tu nombre de usuario de Instagram
 contraseña.send_keys("") # Tu contraseña de Instagram
 ingresar = WebDriverWait(nav, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, "button[type=submit]"))).click()
-# Probablemente tengas que correr la linea 30 dos veces, dependiendo de la cantidad de pop ups que tengas
+# Probablemente tengas que correr esta linea dos veces, dependiendo de la cantidad de pop ups que tengas
+ahora_no = WebDriverWait(nav, 10).until(ec.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Ahora no')]"))).click()
+time.sleep(1)
 ahora_no = WebDriverWait(nav, 10).until(ec.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Ahora no')]"))).click()
 
 # -----INGRESAR A LA BARRA DE BÚSQUEDA----- #
-# La linea 35 tiene que estar contemplada porque, al menos en mi caso, Instagram no muestra la barra de búsqueda
-# por default, sino que primero hay que clickearla para que se muestre y recién ahí ingresar el texto
+# Tuve que agregar esta linea porque, por default, no me aparecía la barra de búsqueda, sino que primero
+# debía clickear el <span> que está en la barra para luego poder ingresar el término de búsqueda
 click_busqueda = WebDriverWait(nav, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, "span[class='TqC_a']"))).click()
 busqueda = WebDriverWait(nav, 10).until(ec.element_to_be_clickable((By.XPATH, "//input[@placeholder='Buscar']")))
 busqueda.clear()
@@ -45,24 +48,27 @@ busqueda.send_keys(Keys.ENTER)
 # Probablemente tengas que repetir el scroll de la página varias veces más, dado que Instagram va cargando las
 # imágenes conforme nosotros vamos haciendo scroll en la página
 def scroll():
-    time.sleep(2)
+    time.sleep(3)
     nav.execute_script("window.scrollTo(0,8550);")
-
-for i in range(5):
-    scroll()
-
-imagenes = nav.find_elements_by_tag_name("img")
-imagenes = [i.get_attribute("src") for i in imagenes]
 
 # -----DESCARGAR LAS IMÁGENES----- #
 # Usa el nombre del término de búsqueda para nombrar la carpeta de origen y las fotos descargadas
-time.sleep(3)
+time.sleep(2)
 ruta = os.getcwd()
 ruta = os.path.join(ruta, nombre + " fotos")
 os.mkdir(ruta)
 
-n = 1
-for i in imagenes:
-    guardar = os.path.join(ruta, nombre + str(n) + ".jpg")
-    wget.download(i, guardar)
-    n += 1
+def guardar_imagenes():
+    n = 1
+    time.sleep(1)
+    imagenes = nav.find_elements_by_tag_name("img")
+    imagenes = [i.get_attribute("src") for i in imagenes]
+    for j in imagenes:
+        guardar = os.path.join(ruta, nombre + str(n) + ".jpg")
+        wget.download(j, guardar)
+        n += 1
+
+for k in range(3):
+    scroll()
+    scroll()
+    guardar_imagenes()
